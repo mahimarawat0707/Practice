251e3d6997526117c93dd6ac928ee0b0526b7e3b
Mahima Rawat <mahimarawat0707@gmail.com>
2026-04-25 22:42:10 +0530

    Add Operators class demonstrating Java operators

---
+++ operators.java
@@ -0,0 +1,26 @@
+class Operators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+ 
+        System.out.println("\nInc/Dec Operators:");
+        System.out.println("++a = " + (++a));
+        System.out.println("--b = " + (--b));
+
+        System.out.println("\nRelational Operators:");
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= b : " + (a > b));
+        System.out.println("a <= b : " + (a < b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+    }
+}
