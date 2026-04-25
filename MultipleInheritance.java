842b63509b40c6b1908d85ae63641c6d90bc2761
Mahima Rawat <mahimarawat0707@gmail.com>
2026-04-25 22:41:16 +0530

    Implement multiple inheritance using interfaces in Java

---
+++ MultipleInheritance.java
@@ -0,0 +1,25 @@
+interface Studying {
+    void study();
+}
+
+interface Playing {
+    void play();
+}
+
+class Student implements Studying, Playing {
+    public void study() {
+        System.out.println("Student is studying.");
+    }
+ 
+    public void play() {
+        System.out.println("Student is playing.");
+    }
+}
+                           
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.study();
+        s.play();
+    }
+}
